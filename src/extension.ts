96d95025576c53209d7fdf0a5dad5f90cdf4894c
Makar <makar@imandra.ai>
2026-02-05 21:49:14 +0800

    don't check for update when testing

--- src/extension.ts
+++ src/extension.ts
@@ -25,18 +25,6 @@ export async function activate(context: ExtensionContext) {
 
   if (imandraxLanguageClient.configuration.isFoundPath(languageClientConfig)) {
 
-    const versionOutdated = await installer.checkVersion();
-    const installedByVscode = await installer.checkForMarker();
-
-    if (versionOutdated && installedByVscode) {
-      console.log('ImandraX binary is outdated, updating...');
-      const args = { revealSetting: { key: "imandrax.lsp.binary", edit: true } };
-      const openUri = Uri.parse(
-        `command:workbench.action.openWorkspaceSettingsFile?${encodeURIComponent(JSON.stringify(args))}`
-      );
-      await installer.promptToInstall(openUri, true);
-    }
-
     const languageClientWrapper_ = new imandraxLanguageClient.ImandraXLanguageClient(getConfig);
     const getClient: () => LanguageClient = () => { return languageClientWrapper_.getClient(); };
 
@@ -77,5 +65,19 @@ export async function activate(context: ExtensionContext) {
   }
   if (context.extensionMode === ExtensionMode.Test || context.extensionMode === undefined) {
     (global as any).testExtensionContext = context;
+  }
+
+  if (context.extensionMode !== ExtensionMode.Test) {
+    const versionOutdated = await installer.checkVersion();
+    const installedByVscode = await installer.checkForMarker();
+
+    if (versionOutdated && installedByVscode) {
+      console.log('ImandraX binary is outdated, updating...');
+      const args = { revealSetting: { key: "imandrax.lsp.binary", edit: true } };
+      const openUri = Uri.parse(
+        `command:workbench.action.openWorkspaceSettingsFile?${encodeURIComponent(JSON.stringify(args))}`
+      );
+      await installer.promptToInstall(openUri, true);
+    }
   }
 }
